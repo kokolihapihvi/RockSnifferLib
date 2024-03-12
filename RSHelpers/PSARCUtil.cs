6b12987634810f463ee39fade3024f144a50648e
tjwrona <tjwrona1992@gmail.com>
2024-03-12 15:48:05 -0400

    Fixed some issues where charts could get the wrong note count
    Beyond Creation - Omnipresent Perception
    Danzig - The Hunter
    Sublime - Badfish

--- RSHelpers/PSARCUtil.cs
+++ RSHelpers/PSARCUtil.cs
@@ -1,6 +1,7 @@
 ﻿using Rocksmith2014PsarcLib.Psarc;
 using Rocksmith2014PsarcLib.Psarc.Asset;
 using Rocksmith2014PsarcLib.Psarc.Models.Json;
+using Rocksmith2014PsarcLib.Psarc.Models.Sng;
 using RockSnifferLib.Logging;
 using RockSnifferLib.Sniffing;
 using System;
@@ -51,6 +52,43 @@ namespace RockSnifferLib.RSHelpers
                     Thread.Sleep(100);
                 }
             }
+        }
+
+        internal static bool isNoteIgnored(Note note)
+        {
+            // Skip notes explicitly marked as "ignored" (notes with the mask 0x40000 set)
+            // These notes do not need to be above the 22nd fret to be ignored.
+            if ((note.NoteMask & 0x40000) != 0)
+            {
+                return true;
+            }
+
+            // FretId of 255 indicates a chord
+            else if (note.FretId != 255)
+            {
+                // Rocksmith ignores notes above the 22nd fret (fret ID of 255 indicates a chord)
+                if (note.FretId > 22)
+                {
+                    return true;
+                }
+
+                // Rocksmith also ignores notes below 22 that slide to a note above 22
+                // Note a value of 255 indicates no slide
+                else if ((note.SlideTo != 255 && note.SlideTo > 22) || (note.SlideUnpitchTo != 255 && note.SlideUnpitchTo > 22))
+                {
+                    return true;
+                }
+
+                // If the note is linked next and also forms an unpitched slide (notes with the mask 0x8000000 set are linked next), ignore it
+                // UNLESS the note is ALSO marked as a pitched slide (happens in Knights of Cydonia for some reason...)
+                // Technically the NEXT note is the one that is ignored, but ignoring this note is simpler and results in the same note count
+                if ((note.NoteMask & 0x8000000) != 0 && note.SlideUnpitchTo != 255 && note.SlideTo == 255)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         /// <summary>
@@ -219,8 +257,9 @@ namespace RockSnifferLib.RSHelpers
 
                             var arr = arrangementSng.Arrangements[maxDifficulty];
 
-                            foreach (var note in arr.Notes)
+                            for (var i = 0; i < arr.Notes.Length; ++i)
                             {
+                                var note = arr.Notes[i];
                                 if (note.Time >= startTime && note.Time < endTime)
                                 {
                                     // Information about note masks
@@ -372,13 +411,13 @@ namespace RockSnifferLib.RSHelpers
 
                                     // Skip notes explicitly marked as "ignored" (notes with the mask 0x40000 set)
                                     // These notes do not need to be above the 22nd fret to be ignored.
-                                    if ((note.NoteMask & 0x40000) != 0)
+                                    if (isNoteIgnored(note))
                                     {
                                         continue;
                                     }
 
                                     // FretId of 255 indicates a chord
-                                    else if (note.FretId == 255)
+                                    if (note.FretId == 255)
                                     {
                                         // If the chord contains any notes over the 22nd fret this does NOT mean it will be ignored
                                         // This definitely seems like a bug in Rocksmith itself, but this seems to be the way the game works
@@ -413,12 +452,13 @@ namespace RockSnifferLib.RSHelpers
                                         var chordNotesID = note.ChordNotesId;
                                         if (chordNotesID != -1)
                                         {
+                                            var linkNextOffset = 1;
                                             var chordNotes = arrangementSng.ChordNotes[chordNotesID];
-                                            for (var i = 0; i < chordNotes.NoteMask.Length; i++)
+                                            for (var j = 0; j < chordNotes.NoteMask.Length; ++j)
                                             {
-                                                var noteMask = chordNotes.NoteMask[i];
-                                                var slideTo = chordNotes.SlideTo[i];
-                                                var slideUnpitchTo = chordNotes.SlideUnpitchTo[i];
+                                                var noteMask = chordNotes.NoteMask[j];
+                                                var slideTo = chordNotes.SlideTo[j];
+                                                var slideUnpitchTo = chordNotes.SlideUnpitchTo[j];
 
                                                 // If the chord contains any notes that slide over the 22nd fret, ignore it
                                                 // Note a value of 255 indicates no slide
@@ -439,15 +479,28 @@ namespace RockSnifferLib.RSHelpers
                                                 // Unlike single notes, this doesn't apply to chords
                                                 //if ((noteMask & 0x8000000) != 0 && slideUnpitchTo != 255) { }
 
-                                                // This is the odd one... if the chord contains any note over 22 and any note that is tremolo, bent, or vibrato
+                                                // This is the odd one... if the chord contains any note over 22 and any note that is muted, tremolo, bent, or vibrato
+                                                // Mask 0x20000 is muted
                                                 // Mask 0x10 is tremolo
                                                 // Mask 0x1000 is bent
                                                 // Mask 0x10000 is vibrato
                                                 // The bend does NOT have to be on the same note that is over 22
-                                                if (((noteMask & 0x10) != 0 || (noteMask & 0x1000) != 0 || (noteMask & 0x10000) != 0) && chordOver22)
+                                                if (((noteMask & 0x20000) != 0 || (noteMask & 0x10) != 0 || (noteMask & 0x1000) != 0 || (noteMask & 0x10000) != 0) && chordOver22)
                                                 {
                                                     ignore = true;
                                                     break;
+                                                }
+
+                                                // If the chord is linked next to an ignored note, ignore it
+                                                // (notes with the mask 0x8000000 set are linked next)
+                                                if ((noteMask & 0x8000000) != 0)
+                                                {
+                                                    var linkedNote = arr.Notes[i + linkNextOffset++];
+                                                    if (isNoteIgnored(linkedNote))
+                                                    {
+                                                        ignore = true;
+                                                        break;
+                                                    }
                                                 }
                                             }
                                         }
@@ -456,27 +509,6 @@ namespace RockSnifferLib.RSHelpers
                                         {
                                             continue;
                                         }
-                                    }
-
-                                    // Rocksmith ignores notes above the 22nd fret (fret ID of 255 indicates a chord)
-                                    else if (note.FretId > 22)
-                                    {
-                                        continue;
-                                    }
-
-                                    // Rocksmith also ignores notes below 22 that slide to a note above 22
-                                    // Note a value of 255 indicates no slide
-                                    else if ((note.SlideTo != 255 && note.SlideTo > 22) || (note.SlideUnpitchTo != 255 && note.SlideUnpitchTo > 22))
-                                    {
-                                        continue;
-                                    }
-
-                                    // If the note is linked next and also forms an unpitched slide (notes with the mask 0x8000000 set are linked next), ignore it
-                                    // UNLESS the note is ALSO marked as a pitched slide (happens in Knights of Cydonia for some reason...)
-                                    // Technically the NEXT note is the one that is ignored, but ignoring this note is simpler and results in the same note count
-                                    if ((note.NoteMask & 0x8000000) != 0 && note.SlideUnpitchTo != 255 && note.SlideTo == 255)
-                                    {
-                                        continue;
                                     }
 
                                     // Note should be counted
@@ -504,6 +536,13 @@ namespace RockSnifferLib.RSHelpers
                             totalNotes = 1479;
                         }
 
+                        // The lead path for Badfish by Sublime gets the wrong note count (off by 1). I dug into this issue THOROUGHLY and could not for the life of me figure out why.
+                        // Like a bad programmer I will hardcode this for now lol. Hopefully I will encounter something similar in the future and will be able to find a pattern.
+                        if (arrangement_id == "B21F30DD94B887B5D4C0E4631D0A511F")
+                        {
+                            totalNotes = 402;
+                        }
+
                         //Build arrangement details
                         var arrangementDetails = new ArrangementDetails
                         {
@@ -516,7 +555,7 @@ namespace RockSnifferLib.RSHelpers
                             isAlternateArrangement = (arrangement.ArrangementProperties.Represent == 0),
                             totalNotes = totalNotes,
                             noteDataHash = Convert.ToHexString(noteDataHash.GetHashAndReset())
-                    };
+                        };
 
                         //Determine path type
                         if (arrangement.ArrangementProperties.PathLead == 1)
